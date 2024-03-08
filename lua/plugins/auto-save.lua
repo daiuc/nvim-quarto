f36c3ab5ca104c946054c2aa2b58cd8384a982fa
daiuc <chaodai@uchicago.edu>
2024-03-07 21:52:44 -0600

    fix auto-save

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -3,7 +3,6 @@ return {
     'pocco81/auto-save.nvim',
     enabled = true,
     opt = {
-      auto_save = true,
       debounce_delay = 30000, -- autosave at most every x milliseconds
     },
   },
