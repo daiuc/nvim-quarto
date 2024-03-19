ee9c2ea71749811d5fb832d070b7991066e15a00
daiuc <chaodai@uchicago.edu>
2024-03-19 12:07:06 -0500

    added chatgpt and replaced g leader key for gpt

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
+}
